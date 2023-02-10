3f87804331759a84b2a3d17d161bcae85703b7b7
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-02-10 15:59:44 +0100

    create mode, controller, db ecc but doesn't work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,9 +32,6 @@
                         <h1 class="display-5 fw-bold">
                             Welcome to Laravel+Bootsrap 5
                         </h1>
-
-                        <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-                        <button class="btn btn-primary btn-lg align-self-start" type="button">Example button</button>
                     </div>
                 </div>
             </div>
