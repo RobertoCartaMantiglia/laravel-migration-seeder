830ac23b242b0dad2ac758ae0e20d6a7c8f60f68
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-02-10 17:48:11 +0100

    trying to filter trains

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -12,7 +12,7 @@ class TrainController extends Controller
 
     public function index()
     {
-        $trains = Train::all();
+        $trains = Train::where('orario_partenza', '=', '2023-02-11');
         dump($trains);
         return view('home', compact('trains'));
     }
